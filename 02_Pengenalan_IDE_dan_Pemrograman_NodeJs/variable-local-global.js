88034da09a07e6d31589014227e7c37966c99360
kholilapras <kholilapras@gmail.com>
2025-03-12 20:04:13 +0700

    Pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,8 @@
+let globalVariable = "Kholil Abdi Prasetiyo";
+
+function getMyIdentify() {
+    let localVariable = "2211104071"
+    console.log(`Nama saya ${globalVariable} dan NIM saya ${localVariable}`);
+}
+console.log(globalVariable);
+getMyIdentify();
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,17 @@
+// var
+
+var name = "Kholil Abdi Prasetiyo";
+const nim  = 2211104071
+
+console.log(name);
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log("Nama saya adalah ${name} dan nim saya adalah ${nim}")
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Kholil Abdi Prasetiyo"
